54ac3631e065de18207693565721f6fa1a035034
Bedour47 <bedour.ir@gmail.com>
2019-07-21 16:02:29 +0300

    user can post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :delete]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :delete]
   def index
     @posts = Post.all
   end
@@ -8,12 +8,37 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = current_user.posts.create(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        # format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:image, :caption, :user)
   end
 end
