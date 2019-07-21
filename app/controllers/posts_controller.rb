c55de5353238b470652c37f63082310001b057f4
Bedour47 <bedour.ir@gmail.com>
2019-07-21 15:01:53 +0300

    add authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :delete]
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def show
