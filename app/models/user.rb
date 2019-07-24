7239f9b747a4f2b57ff4a11bd61585d07fade1f4
Bedour47 <bedour.ir@gmail.com>
2019-07-24 11:10:47 +0300

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :likes, dependent: :destroy
+
+  validates :email, :username, :name, :password, presence: true
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+  validates :name, :username, length: {
+    minimum: 3,
+    #customize the error message. The %{count}, rails provide it to us from the minimum
+    too_short: "must have at least %{count} letters"
+}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
